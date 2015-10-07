ab6f22302fd36e8b61b5b17dddd18dea150b3212
Ren <renardchien@gmail.com>
2015-10-07 10:49:23 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+//import libraries 
+var path = require('path'); //path is a built-in node library to handle file system paths
+var express = require('express'); //express is a popular Model-View-Controller framework for Node
+
+
---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+var path = require('path'); //path is a built-in node library to handle file system paths
+
+//There is no reason for the name here except as an example of how to set something for the POST
+var name = "unknown"; //We will set and get an arbitrary name
+
